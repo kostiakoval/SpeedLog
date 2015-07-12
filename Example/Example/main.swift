6dec666ebc7506470d252976c890dd1b2e78e1cb
Kostiantyn Koval <konstantin.koval1@gmail.com>
2015-07-12 10:18:51 +0200

    blog example

--- Example/Example/main.swift
+++ Example/Example/main.swift
@@ -2,11 +2,46 @@
 //  main.swift
 //  Example
 //
-//  Created by Kostiantyn Koval on 12/07/15.
+//  Created by Kostiantyn Koval on 09/07/15.
 //  Copyright (c) 2015 Kostiantyn Koval. All rights reserved.
 //
 
 import Foundation
 
-println("Hello, World!")
-SpeedLog.print("Hello")
+//V1
+/*for i in 0...100 {
+let a = i * 2
+println(a)
+} */
+
+//V2
+/*func printNums() {
+for i in 0...100 {
+let a = i * 2
+println(a)
+}
+}*/
+
+//V3
+func printNums() {
+  for i in 0...100 {
+    let a = i * 2
+    #if DEBUG
+      println(a)
+    #endif
+  }
+}
+
+//V4
+func speedPrintNums() {
+  for i in 0...100 {
+    let a = i * 2
+    SpeedLog.println(a)
+    SpeedLog.print(a)
+  }
+}
+
+
+
+//printNums()
+speedPrintNums()
