04063882aa77904603a4500569feab8fc8195996
phimage <eric.marchand.n7@gmail.com>
2015-10-26 10:42:26 +0100

    More sweat syntax for options union and intersect

--- SpeedLog/SpeedLog.swift
+++ SpeedLog/SpeedLog.swift
@@ -28,7 +28,7 @@ public struct LogMode : OptionSetType {
   public static var Line: LogMode        { return self.init(1 << 2) }
 
 //Helper
-  public static var AllOptions: LogMode  { return FileName.union(FuncName).union(Line) }
+  public static var AllOptions: LogMode  { return [FileName, FuncName, Line] }
 }
 
 public struct SpeedLog {
@@ -53,16 +53,16 @@ public struct SpeedLog {
 private extension SpeedLog {
   static func printStringForMode<T>(object: T, file: String, function: String, line: Int) -> String {
     var result: String = ""
-    //print("\(filename).\(function)[\(line)]: \(object)\n")
-    if mode.intersect(.FileName) != [] {
+    //print("\(filename).\(function)[\(line)]: \(object)")
+    if mode.contains(.FileName) {
         
       let filename = file.lastPathComponent.stringByDeletingPathExtension
       result = "\(filename)."
     }
-    if mode.intersect(.FuncName) != [] {
+    if mode.contains(.FuncName) {
       result += "\(function)"
     }
-    if mode.intersect(.Line) != [] {
+    if mode.contains(.Line) {
       result += "[\(line)]"
     }
     
