f47b6a3e086466492bd7f497e43c5de17ee9c374
Kostiantyn Koval <konstantin.koval1@gmail.com>
2015-10-26 21:24:49 +0100

    add Pods and pod path for travis

---
+++ Example/Pods/Target Support Files/SpeedLog/SpeedLog-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_SpeedLog : NSObject
+@end
+@implementation PodsDummy_SpeedLog
+@end
---
+++ Example/Pods/Target Support Files/SpeedLog/SpeedLog-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double SpeedLogVersionNumber;
+FOUNDATION_EXPORT const unsigned char SpeedLogVersionString[];
+
